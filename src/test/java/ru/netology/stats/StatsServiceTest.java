a8f7e44ff359b391109f7c409a7b00ec24e8663d
Anna Kuznetsova <akry67@gmail.com>
2021-11-05 16:24:37 +0400

    dop

---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,20 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatsServiceTest {
+
+    @Test
+    void shouldSumSales() {
+        StatsService service = new StatsService();
+
+        int[] enterpriseSales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 180;
+
+        int actual = service.sumSales(enterpriseSales);
+
+        assertEquals(expected, actual);
+    }
+}
