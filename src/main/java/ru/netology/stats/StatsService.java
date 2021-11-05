68dd07df01cf967c1c56f1957a6e45ad1923baab
Anna Kuznetsova <akry67@gmail.com>
2021-11-05 17:40:33 +0400

    6 program

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -13,4 +13,54 @@ public class StatsService {
     public int averageCompanySales(int[] sales){
         return sumSales(sales)/sales.length;
     }
+
+    public int maxSales(long[] sales){
+        int maxMonth =0;
+        int month = 0;
+        for (long sale : sales) {
+            if (sale >= sales[maxMonth]){
+                maxMonth = month;
+            }
+            month = month +1;
+        }
+        return maxMonth +1;
+    }
+
+    public int minSales(long[] sales) {
+        int minMonth = 0;
+        int month = 0; // переменная для индекса рассматриваемого месяца в массиве
+        for (long sale : sales) {
+            // sales[minMonth] - продажи в месяце minMonth
+            // sale - продажи в рассматриваемом месяце
+            if (sale <= sales[minMonth]) {
+                minMonth = month;
+            }
+            month = month + 1; // следующий рассматриваемый месяц имеет номер на 1 больше
+        }
+        return minMonth + 1;
+    }
+
+    //Кол-во месяцев, в которых продажи были ниже среднего (см. п.2)
+    public int monthsMinAverageCompanySales (int[] sales){
+        int counter = 0;
+        int numberMonthMinAvr =0;
+        for (int sale : sales) {
+            if (sale < sumSales(sales)/sales.length){
+                numberMonthMinAvr = counter ++;
+            }
+        }
+        return counter;
+    }
+
+    //Кол-во месяцев, в которых продажи были выше среднего (см. п.2)
+    public int monthMaxAverageCompanySales (int[] sales){
+        int counter = 0;
+        int numberMonthMaxAvr =0;
+        for (int sale : sales) {
+            if (sale > sumSales(sales)/sales.length){
+                numberMonthMaxAvr = counter ++;
+            }
+        }
+        return counter;
+    }
 }
--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -29,4 +29,51 @@ class StatsServiceTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    void shouldMaxSales() {
+        StatsService service = new StatsService();
+
+        long[] enterpriseSales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 8;
+
+        long actual = service.maxSales(enterpriseSales);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldMinSales() {
+        StatsService service = new StatsService();
+
+        long[] enterpriseSales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 9;
+
+        long actual = service.minSales(enterpriseSales);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void shouldMonthsMinAverageCompanySales() {
+        StatsService service = new StatsService();
+
+        int[] enterpriseSales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+
+        int actual = service.monthsMinAverageCompanySales(enterpriseSales);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldMonthsMaxAverageCompanySales() {
+        StatsService service = new StatsService();
+
+        int[] enterpriseSales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+
+        int actual = service.monthMaxAverageCompanySales(enterpriseSales);
+
+        assertEquals(expected, actual);
+    }
 }
